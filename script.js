025a6da18f39dcdae6d545dc7590b427d38a4065
Saad Aulakh <saadaulakh04@gmail.com>
2022-06-23 16:14:35 -0400

    Updated index.html, script.js, and styles.css

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+
+let board = document.querySelector('.board')
+
+function populate(boardSize){
+    board.style.gridTemplateColumns = `repeat(${boardSize} ,1fr)`;
+    board.style.gridTemplateRows = `repeat(${boardSize} ,1fr)`;
+    let amount = boardSize*boardSize;
+    
+    for(let i=0; i<amount; i++){
+        const square = document.createElement('div');
+        square.style.backgroundColor = 'white';
+        square.addEventListener('mouseover', changeColor)
+        square.addEventListener('mousedown', changeColor)
+        board.appendChild(square);
+    }
+}
+
+function changeColor(e){
+    if(e === 'mouseover' && !(e === 'mousedown')){
+        return;
+    }
+    
+}
+
+populate(16)
+
 var slider = document.getElementById("slider")
 var size = document.getElementById("size")
-
 size.innerHTML = `${slider.value}x${slider.value}`;
 
 slider.oninput = function(){
